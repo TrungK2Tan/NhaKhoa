261e2ef2be4c1f06395a4fda277ad5833ebca8fc
TrungK2Tan <trungtan25112k2@gmail.com>
2023-11-23 04:36:45 +0700

    LichKham nha si(Areas NhaSI)

--- Areas/NhaSi/Controllers/QLLichController.cs
+++ Areas/NhaSi/Controllers/QLLichController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PagedList;
 namespace NhaKhoa.Areas.NhaSi.Controllers
 {
     public class QLLichController : Controller
@@ -20,21 +20,54 @@ namespace NhaKhoa.Areas.NhaSi.Controllers
             var user = db.AspNetUsers.Find(currentUserId);
             ViewBag.TenNhaSi = user.FullName;
             ViewBag.HinhAnh = user.HinhAnh;
-            // Retrieve the ThoiKhoaBieu for the logged-in dentist
-            var thoikhoabieus = db.ThoiKhoaBieux
-                                 .Where(tkb => tkb.Id_Nhasi == currentUserId)
-                                 .ToList();
-            return View(thoikhoabieus);
+            try
+            {
+                // Filter ThoiKhoaBieu based on the Id_Nhasi
+                var thoiKhoaBieu = db.Thus.Where(u => u.ThoiKhoaBieux.Any(r => r.Id_Nhasi == currentUserId)).ToList();
+
+
+                if (thoiKhoaBieu == null || !thoiKhoaBieu.Any())
+                {
+                    // Xử lý khi không có dữ liệu
+                    return View("ErrorView"); // Thay "ErrorView" bằng tên view hiển thị thông báo lỗi
+                }
+
+                return View(thoiKhoaBieu);
+            }
+            catch (Exception)
+            {
+                // Xử lý exception, log và hiển thị thông báo lỗi
+                return View("ErrorView"); // Thay "ErrorView" bằng tên view hiển thị thông báo lỗi
+            }
         }
-        public ActionResult ViewCalendar()
+        public ActionResult ViewCalendar(int? page)
         {
             // Get the currently logged-in user's ID
             string currentUserId = User.Identity.GetUserId();
             var user = db.AspNetUsers.Find(currentUserId);
             ViewBag.TenNhaSi = user.FullName;
             ViewBag.HinhAnh = user.HinhAnh;
-            var thoikhoabieus = db.ThoiKhoaBieux.ToList();
-            return View(thoikhoabieus);
+            try
+            {
+                var thoiKhoaBieu = db.Thus.ToList();
+
+                if (!thoiKhoaBieu.Any())
+                {
+                    ViewBag.ErrorMessage = "Không có dữ liệu để hiển thị.";
+                    return View(thoiKhoaBieu.ToPagedList(1, 7)); // Mặc định hiển thị trang 1, mỗi trang 10 phần tử
+                }
+
+                int pageSize = 7; // Số lượng mục hiển thị trên mỗi trang
+                int pageNumber = (page ?? 1);
+
+                return View(thoiKhoaBieu.ToPagedList(pageNumber, pageSize));
+            }
+            catch (Exception)
+            {
+                ViewBag.ErrorMessage = "Đã xảy ra lỗi khi lấy dữ liệu. Vui lòng thử lại sau.";
+
+                return View("ErrorView");
+            }
         }
     }
 }
