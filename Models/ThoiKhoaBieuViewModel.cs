5b12066da5d67d4b9d4b672694b8605f122e4274
TrungK2Tan <trungtan25112k2@gmail.com>
2023-11-27 23:19:59 +0700

    Admin quản lí lịch làm việc cho Nha sĩ

---
+++ Models/ThoiKhoaBieuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PagedList;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NhaKhoa.Models
+{
+    public class ThoiKhoaBieuViewModel
+    {
+        public List<Thu> DanhSachThu { get; set; }
+        public List<ThoiKhoaBieu> DanhSachThoiKhoaBieu { get; set; }
+        public DateTime[] weeks { get; set; }
+        public DateTime SelectedWeek { get; set; }
+
+        public List<ThoiKhoaBieu> WorkSchedules { get; set; }
+    }
+}
